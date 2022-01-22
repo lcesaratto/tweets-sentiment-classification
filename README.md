154c19b6a547718ee24da95641b7007a2e057efc
Lisandro Cesaratto <lisandrocesaratto95@gmail.com>
2022-01-22 18:27:32 +0100

    project upload

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Tweets-Sentiment-DeBERTa
+
+## ML4NLP WiSe 2021/22 Challenge
+
+### Usage
+
+```bash
+# clone https://github.com/jasonwei20/eda_nlp into 'eda_nlp-master' and place it in root folder
+
+# download the repository to your local machine
+git clone git@github.com:lcesaratto/Tweets-Sentiment-DeBERTa.git
+
+# move into repository
+cd Tweets-Sentiment-DeBERTa
+
+# run prediction
+execute __main__.py file in folder lib
+(select variables accordingly)
+```
+
+### Config
+
+```json
+{
+  "data": {
+    "train_path": "data/train.csv",
+    "eval_path": "data/eval.csv",
+    "test_path": null
+  },
+  "preprocess": [
+    "hyperlinks",
+    "mentions",
+    "hashtags",
+    "retweet",
+    "repetitions",
+    "emojis",
+    "smileys",
+    "spaces"
+  ],
+  "model": {
+    "name": "microsoft/deberta-base"
+  },
+  "trainer": {
+    "learning_rate": 2e-5,
+    "per_device_train_batch_size": 16,
+    "per_device_eval_batch_size": 16,
+    "weight_decay": 0.01,
+    "output_dir": "results/",
+    "logging_dir": "logs/"
+  }
+}
+```
+
+### Credits
+
+- Lisandro A. Cesaratto: <https://github.com/lcesaratto>
+- Simon Münker: <https://github.com/smnmnkr>
---
+++ lib/__main__.py
@@ -0,0 +1,54 @@
+import torch
+from transformers import AutoModelForSequenceClassification
+import warnings
+warnings.filterwarnings("ignore")
+
+from train import train_model_on_train_data, train_model_on_full_train_data
+from evaluate import evaluate_on_test_data
+
+
+# Parameters
+TRAIN_DATA_PATH = "data/train.csv"
+TEST_DATA_PATH = "data/eval.csv"
+MODEL_NAME = "microsoft/deberta-base"
+BATCH_SIZE = 16
+NUM_EPOCHS = 1
+SEED = 42
+TRAIN_MODEL_ON_FULL_TRAINING_DATA = True
+SAVE_NEW_MODEL = True
+USE_PRETRAINED_MODEL = True
+DOWNLOAD_WEIGHTS = True
+
+
+if USE_PRETRAINED_MODEL:
+    if DOWNLOAD_WEIGHTS:
+        import gdown
+        print("Downloading weights")
+        id = "1rvml7MaEHOnd5TxYD7eAaHPJPePNKYGG"
+        output = "pretrained_model/model_state_dict.pt"
+        gdown.download( output=output, quiet=False, id=id)
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+            MODEL_NAME,
+            num_labels = 2,
+            output_attentions = False,
+            output_hidden_states = False,
+        )
+    model.load_state_dict(torch.load("pretrained_model/model_state_dict.pt"))
+
+else:
+    if TRAIN_MODEL_ON_FULL_TRAINING_DATA:
+        model, training_stats = train_model_on_full_train_data(TRAIN_DATA_PATH, MODEL_NAME, BATCH_SIZE, NUM_EPOCHS, SEED)
+    else:
+        model, training_stats = train_model_on_train_data(TRAIN_DATA_PATH, MODEL_NAME, BATCH_SIZE, NUM_EPOCHS, SEED)
+    print("\nTraining results: ", training_stats)
+
+    if SAVE_NEW_MODEL:
+        torch.save(model.state_dict(), "pretrained_model/model_state_dict.pt")
+        torch.save(model, "pretrained_model/entire_model.pt")
+
+
+
+testing_stats = evaluate_on_test_data(model, TEST_DATA_PATH, MODEL_NAME, BATCH_SIZE, SEED)
+print("\nTesting results: ", testing_stats)
+
